b7aac99d6ed4b4dd4b6a743a62bc694a7fbb0da5
hakobyanviktoria8 <hakobyanviktorya8@gmail.com>
2022-06-29 23:01:58 +0400

    create login form and give data

--- src/App.tsx
+++ src/App.tsx
@@ -1,56 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
-import './App.css';
+import { useState } from 'react';
+import './App.scss';
 
 function App() {
+  const [formData, setFormData] = useState({email: '', password: ''})
+
+  const handleChange = (e: any) => {
+    const { name, value } = e.target
+    setFormData({
+       ...formData,
+       [name]: value,
+    })
+  }
+
+  const handleSubmit = (e: any) => {
+    e.preventDefault();
+    console.log("formData",formData);    
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
+    <div className="app">
+      <div className='formWrapper'>
+        <h2>Log in</h2>
+        <form onSubmit={handleSubmit}>
+            <label>
+              Email:
+              <input 
+                type="text" 
+                name="email" 
+                onChange={handleChange} 
+                placeholder="Email" 
+                autoComplete="off"
+              />
+            </label>
+            <label>
+              Password:
+              <input 
+                type="password" 
+                name="password" 
+                onChange={handleChange} 
+                placeholder="Password" 
+                autoComplete="off"
+              />
+            </label>
+            <input 
+              type="submit" 
+              value="Log in" 
+              className='logInButton'
+            />
+          </form>
+        </div>
     </div>
   );
 }
