753a6cefbd2c070db8ce9e73e71897bbc50fe9f3
hakobyanviktoria8 <hakobyanviktorya8@gmail.com>
2022-07-02 02:25:07 +0400

    Added modal component

--- src/api/index.tsx
+++ src/api/index.tsx
@@ -1,4 +1,5 @@
 const API_URL = 'https://magic-dashboard-api.herokuapp.com/api'
+const MAIN_URL = 'magic-dashboard-api.herokuapp.com/api/v1/users'
 
 // login
 export function logInApi(data: any) {
@@ -45,5 +46,29 @@ export function userIdApi(token: string | any, userId: string) {
          'Authorization': `Bearer ${token}`
       },
       method: 'GET',
+   })
+}
+
+// update user data
+export function updateUserDataApi(token: string | any, userId: string) {
+   return fetch(`${ API_URL }/v1/users/update/${userId}`, {
+      method: 'PUT',
+      headers: { 
+         'authority': `${MAIN_URL}`,
+         'Accept': 'application/json',
+         'Authorization': `Bearer ${token}`,
+         'Content-Type': 'application/json',
+         'origin': `https://${MAIN_URL}`,
+         'referer': `https://${MAIN_URL}`,
+      },
+      body: JSON.stringify({ 
+         name: 'Allooo',
+         language_id: 1,
+         min_bet: 1000,
+         max_bet: 150000,
+         user_level: 'normal',  
+         password: 'Yrngfp9#kX@U=J9L',
+         password_confirmation: 'Yrngfp9#kX@U=J9L',       
+      })
    })
 }
--- src/pages/userId/UserId.tsx
+++ src/pages/userId/UserId.tsx
@@ -1,5 +1,6 @@
 import { useSelector } from "react-redux";
 import { useParams } from "react-router";
+import { updateUserDataApi } from "../../api";
 import { Layout } from "../../components/Layout";
 import './userId.scss';
 
@@ -7,7 +8,22 @@ export const UserId = () => {
     const params = useParams();
     const userId = params.userId;
     const usersData = useSelector((state: any) => state)
-    let user = usersData?.filter((user: any) => user._id === userId)[0]      
+    let user = usersData?.filter((user: any) => user._id === userId)[0]  
+    
+    const handleClick = async (e: any) => {
+        e.preventDefault();
+        const token = localStorage.getItem('token');
+        try {
+          if(token){
+            const res = await updateUserDataApi(token, user._id)
+            const result = await res.json()
+            console.log("Update user data is ", result, user)
+          }
+        } catch (error) {
+          console.log("Error message is", error);      
+        }
+    }  
+    
   return (
     <Layout> 
         <div className="userId">
@@ -18,6 +34,7 @@ export const UserId = () => {
             <p><b>Balance:</b> {user.balance}</p> 
             <p><b>Master:</b> {user.master}</p> 
             <p><b>Verification:</b> {user.verification}</p> 
+            <button onClick={handleClick}>Edit</button>
         </div>
     </Layout>
   )
